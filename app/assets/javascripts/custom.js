0fe21821283cb453261e53cd5da0f4969cee780d
Naveed Khan <nav6188@gmail.com>
2015-06-28 16:13:22 +0000

    Added validations and contact form

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+var main = function() {
+    
+    $('#contact_comments').keyup(function() {
+        var postLength = $(this).val().length;
+        var charsLeft = 140 - postLength;
+        $('.counter').text(charsLeft);
+    });
+       
+}; 
+
+$(document).ready(main);
